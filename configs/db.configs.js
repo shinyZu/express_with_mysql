7d4618ad3b40b0a534a7dc303223b8782a567aed
shinyZu <shinysirichandra1141@gmail.com>
2022-08-18 17:34:20 +0530

    Performed CRUD operations with Customer table

--- configs/db.configs.js
+++ configs/db.configs.js
@@ -1,8 +1,79 @@
-module.exports = {
-  database: {
-    host: "localhost",
-    user: "root",
-    password: "shiny1234",
-    database: "express_mysql",
-  },
-};
+// module.exports = {
+//   database: {
+//     host: "localhost",
+//     user: "root",
+//     password: "shiny1234",
+//     database: "express_mysql",
+//   },
+// };
+
+//------- Creating MySQL Database Connection ------------
+
+const mysql = require("mysql");
+// const db = require("./configs/db.configs");
+// var connection = mysql.createConnection(db.database);
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "shiny1234",
+  database: "express_mysql",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully Connected to express_mysql database");
+
+    // Create Customer Table
+    var query_customerTable =
+      "CREATE TABLE IF NOT EXISTS customer (customer_id VARCHAR(255) PRIMARY KEY, customer_name VARCHAR(255), customer_address VARCHAR(255), contact_no INT)";
+
+    connection.query(query_customerTable, (err, result) => {
+      if (err) throw err;
+
+      if (result.warningCount === 0) {
+        console.log("Customer Table created");
+      }
+    });
+
+    // Create Item Table
+    var query_itemTable =
+      "CREATE TABLE IF NOT EXISTS item (item_code VARCHAR(255) PRIMARY KEY, description VARCHAR(255), unit_price DOUBLE, qty INT)";
+
+    connection.query(query_itemTable, (err, result) => {
+      if (err) throw err;
+
+      if (result.warningCount === 0) {
+        console.log("Item Table created");
+      }
+    });
+
+    // Create Orders Table
+    var query_orderTable =
+      "CREATE TABLE IF NOT EXISTS orders (order_id VARCHAR(255) PRIMARY KEY, order_date VARCHAR(255), order_cost DATE, customer_id VARCHAR(255), CONSTRAINT FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE CASCADE ON UPDATE CASCADE)";
+
+    connection.query(query_orderTable, (err, result) => {
+      if (err) throw err;
+
+      if (result.warningCount === 0) {
+        console.log("Orders Table created");
+      }
+    });
+
+    // Create OrderDetail Table
+    var query_orderDetailTable =
+      "CREATE TABLE IF NOT EXISTS order_detail (order_id VARCHAR(255), item_code VARCHAR(255), order_qty INT, CONSTRAINT PRIMARY KEY (order_id, item_code), CONSTRAINT FOREIGN KEY (item_code) REFERENCES item(item_code) ON DELETE CASCADE ON UPDATE CASCADE)";
+
+    connection.query(query_orderDetailTable, (err, result) => {
+      if (err) throw err;
+
+      if (result.warningCount === 0) {
+        console.log("OrderDetail Table created");
+      }
+    });
+  }
+});
+
+module.exports = connection;
+//-------------------------------------------------------
--- routes/customer.js
+++ routes/customer.js
@@ -4,24 +4,81 @@ const router = express.Router();
 const app = express();
 app.use(express.json());
 
+const connection = require("../configs/db.configs");
+
 router.get("/", (req, res) => {
-  res.send("get all customers");
+  var query_getAll = "SELECT * FROM customer";
+  connection.query(query_getAll, (err, rows) => {
+    if (err) throw err;
+    res.send(rows);
+  });
 });
 
 router.get("/:id", (req, res) => {
-  res.send("search customer");
+  const id = req.params.id;
+
+  var query = "SELECT * FROM customer WHERE customer_id=?";
+  connection.query(query, [id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
 router.post("/", (req, res) => {
-  res.send(req.body);
+  const body = req.body;
+  const id = body.customer_id;
+  const name = body.customer_name;
+  const address = body.customer_address;
+  const contact = body.contact_no;
+
+  var query =
+    "INSERT INTO customer (customer_id, customer_name , customer_address, contact_no) VALUES (?,?,?,?)";
+
+  //   connection.query(query, [parameters], function to run if query executed);
+  connection.query(query, [id, name, address, contact], (err) => {
+    if (err) {
+      res.send({ message: "Customer Already exists with id : " + id });
+    } else {
+      res.send({ message: "Customer Saved Sucessfully!" });
+    }
+  });
 });
 
 router.put("/", (req, res) => {
-  res.send(req.body);
+  const body = req.body;
+  const id = body.customer_id;
+  const name = body.customer_name;
+  const address = body.customer_address;
+  const contact = body.contact_no;
+
+  // TODO: Duplicate Contact
+
+  var query =
+    "UPDATE customer SET customer_name=?, customer_address=?, contact_no=? WHERE customer_id=?";
+  connection.query(query, [name, address, contact, id], (err, result) => {
+    if (err) throw err;
+
+    if (result.affectedRows) {
+      res.send({ message: "Customer Updated Successfully!" });
+    } else {
+      res.send({ message: "Customer Not Found" });
+    }
+  });
 });
 
 router.delete("/:id", (req, res) => {
-  res.send(req.params.id);
+  const id = req.params.id;
+  var query = "DELETE FROM customer WHERE customer_id=?";
+
+  connection.query(query, [id], (err, result) => {
+    if (err) throw err;
+
+    if (result.affectedRows > 0) {
+      res.send({ message: "Customer Deleted Successfully!" });
+    } else {
+      res.send({ message: "Customer Not Found" });
+    }
+  });
 });
 
 module.exports = router;
